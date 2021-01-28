1ed3c084fe7016025232c9d611085fb5d5c22efc
Hugo Mendonça <hugomendonca9@gmail.com>
2021-01-28 16:09:11 -0300

    Pensando nos testes

--- src/Modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/Modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,9 @@
-test('sum two numbers', () => {
-    expect(1 + 2).toBe(3);
+describe('CreateAppointment', () => {
+    it('should be able to create a new appointment', () => {
+        // test
+    });
+
+    /* it('should not be able to create two appointments on the same time', () => {
+        expect(1 + 2).toBe(3);
+    }); */
 });
