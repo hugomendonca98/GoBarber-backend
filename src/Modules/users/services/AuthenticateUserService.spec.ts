9c9aa6663821ed2225c141bb1474e052b0b8c8a0
Hugo Mendonça <hugomendonca9@gmail.com>
2021-01-31 01:48:04 -0300

    Testando autenticação

---
+++ src/Modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,34 @@
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+
+import FakeUserRepository from '../repositories/fakes/fakeUsersRepository';
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
+
+describe('AuthenticateUser', () => {
+    it('should be able to authenticate', async () => {
+        const fakeUserRepository = new FakeUserRepository();
+        const fakeHashProvider = new FakeHashProvider();
+        const createUser = new CreateUserService(
+            fakeUserRepository,
+            fakeHashProvider,
+        );
+        const autheticanteUser = new AuthenticateUserService(
+            fakeUserRepository,
+            fakeHashProvider,
+        );
+
+        const user = await createUser.execute({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '12345',
+        });
+
+        const response = await autheticanteUser.execute({
+            email: 'johndoe@example.com',
+            password: '12345',
+        });
+
+        expect(response).toHaveProperty('token');
+        expect(response.user).toEqual(user);
+    });
+});
--- src/Modules/users/services/CreateUserService.spec.ts
+++ src/Modules/users/services/CreateUserService.spec.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/Errors/appError';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
 import FakeUserRepository from '../repositories/fakes/fakeUsersRepository';
 import CreateUserService from './CreateUserService';
@@ -6,7 +7,11 @@ import CreateUserService from './CreateUserService';
 describe('CreateUser', () => {
     it('should be able to create a new user', async () => {
         const fakeUserRepository = new FakeUserRepository();
-        const createUser = new CreateUserService(fakeUserRepository);
+        const fakeHashProvider = new FakeHashProvider();
+        const createUser = new CreateUserService(
+            fakeUserRepository,
+            fakeHashProvider,
+        );
 
         const user = await createUser.execute({
             name: 'John Doe',
@@ -19,7 +24,11 @@ describe('CreateUser', () => {
 
     it('should not be able to create a new user with same eamil another', async () => {
         const fakeUserRepository = new FakeUserRepository();
-        const createUser = new CreateUserService(fakeUserRepository);
+        const fakeHashProvider = new FakeHashProvider();
+        const createUser = new CreateUserService(
+            fakeUserRepository,
+            fakeHashProvider,
+        );
 
         await createUser.execute({
             name: 'John Doe',
