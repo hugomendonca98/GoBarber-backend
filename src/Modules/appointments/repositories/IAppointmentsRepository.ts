8db82ad1f1b2290f77af1572ee8534ec0b755b76
Hugo Mendonça <hugomendonca9@gmail.com>
2021-02-06 23:11:36 -0300

    Filtrando agendamentos por mês

--- src/Modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/Modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,11 @@
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 import Appointments from '../infra/typeorm/entities/Appointments';
 
 export default interface IAppointmentsRepository {
     create(data: ICreateAppointmentDTO): Promise<Appointments>;
     findByDate(date: Date): Promise<Appointments | undefined>;
+    findAllInMonthFromProvider(
+        data: IFindAllInMonthFromProviderDTO,
+    ): Promise<Appointments[]>;
 }
